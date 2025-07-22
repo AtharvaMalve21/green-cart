f7a3a547283a108edc5cd9f4c79864af145bbe6e
Atharva Malve <atharvam2103@gmail.com>
2025-07-22 15:00:22 +0530

    updated product-fetch logic on frontend

--- client/src/context/ProductContext.jsx
+++ client/src/context/ProductContext.jsx
@@ -1,9 +1,29 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
+import axios from "axios";
+import { toast } from "react-hot-toast";
 
 export const ProductContext = createContext({});
 
 export const ProductContextProvider = ({ children }) => {
   const [products, setProducts] = useState([]);
+
+  const URI = import.meta.env.VITE_BACKEND_URI;
+
+  const fetchProductDetails = async () => {
+    try {
+      const { data } = await axios.get(URI + "/api/product");
+
+      if (data.success) {
+        setProducts(data.data);
+      }
+    } catch (err) {
+      toast.error(err.response.data.message);
+    }
+  };
+
+  useEffect(() => {
+    fetchProductDetails();
+  }, []);
 
   return (
     <ProductContext.Provider value={{ products, setProducts }}>
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,37 +1,11 @@
-import React, { useContext, useEffect } from "react";
-import { UserContext } from "../context/UserContext";
-
+import React from "react";
 import MainBanner from "../components/MainBanner";
 import Categories from "../components/Categories";
 import BestSeller from "../components/BestSeller";
-
-import axios from "axios";
-import { toast } from "react-hot-toast";
-import { ProductContext } from "../context/ProductContext";
 import BottomBanner from "../components/BottomBanner";
 import NewsLetter from "../components/NewsLetter";
 
 const Home = () => {
-  
-  const { setProducts } = useContext(ProductContext);
-
-  const URI = import.meta.env.VITE_BACKEND_URI;
-
-  const fetchProductDetails = async () => {
-    try {
-      const { data } = await axios.get(URI + "/api/product");
-      if (data.success) {
-        setProducts(data.data);
-      }
-    } catch (err) {
-      toast.error(err.response.data.message);
-    }
-  };
-
-  useEffect(() => {
-    fetchProductDetails();
-  }, []);
-
   return (
     <div className="mt-10">
       <MainBanner />
